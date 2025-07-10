554a2419dce791d51dc4f55127319ae5abe5a9b7
Typical Username <42663452+Typcial-Username@users.noreply.github.com>
2025-07-09 23:23:51 -0700

    Finish lab 6-1, begin on 8.1

--- lab_6-1/lab_6-1.ino
+++ lab_6-1/lab_6-1.ino
@@ -27,31 +27,55 @@ int adcVal = 0;
 float voltVal = 0.0f; 
 float thermVal = 0.0f;
 
-float temp = 0.0f;
+float temp, tempC, tempF = 0.0;
 
-int adcMax = 4096;
-float vcc  = 3.3f;
+float adcMax = 4095;
+float vcc  = 3.3;
 
-int R2 = 30000;
+float R1 = 30000.0;
+float R2 = 0.0f;
+
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+float logR2 = 0.0;
 
 void loop() {
   adcVal = analogRead(THERM_PIN);
 
-  M5.Lcd.setCursor(50, 50);
+  M5.Lcd.setCursor(10, 10);
   M5.Lcd.print("ADC Val: ");
   M5.Lcd.println(adcVal);
 
-  voltVal = (thermVal / adcMax) * vcc;
+  R2 = R1 * (adcMax / adcVal - 1);
 
-  M5.Lcd.setCursor(50, 100);
+  M5.Lcd.setCursor(10, 50);
+  M5.Lcd.print("R2: ");
+  M5.lcd.println(R2);
+
+  voltVal = (adcVal / adcMax) * vcc;
+
+  M5.Lcd.setCursor(10, 90);
   M5.Lcd.print("Voltage: ");
   M5.Lcd.println(voltVal);
 
-  thermVal = (vcc / (R2 * voltVal)) - R2;
+  thermVal = (vcc / (R2 * voltVal)) - R1;
 
-  M5.Lcd.setCursor(50, 150);
+  M5.Lcd.setCursor(10, 130);
   M5.Lcd.print("Thermister Val: ");
   M5.Lcd.println(thermVal);
+
+  logR2 = log(R2);
+  temp = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+
+  tempC = temp - 273.15;
+  tempF = (tempC * 9.0) / 5.0 + 32;
+
+  M5.Lcd.setCursor(10, 170);
+  M5.Lcd.print("Temp: ");
+  M5.Lcd.print(tempF);
+  M5.lcd.print("*F (");
+  M5.Lcd.print(tempC);
+  M5.Lcd.println("*C)");
 
   delay(100);
 }
---
+++ lab_8-1/lab_8-1.ino
@@ -0,0 +1,30 @@
+#include <IOXhop_FirebaseESP32.h>
+#include <IOXhop_FirebaseStream.h>
+
+#include <WiFi.h>
+#include <M5Core2.h>
+
+#include "secrets.h"
+
+void setup() {
+  M5.begin();
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Conntected: ");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
